3fd35155e92d14224cff67fbd77a5349aa5a149b
unknown <mcjannetp@gmail.com>
2025-01-24 15:47:44 -0500

    modified to updated .net to house v8 and 9

--- Fundamentals/obj/Debug/net8.0/Fundamentals.AssemblyInfo.cs
+++ Fundamentals/obj/Debug/net8.0/Fundamentals.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f9e8fe9bfa69de14ec25b9a640477417147212c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
